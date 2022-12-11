3f9c86aff6dcbb0df7fef1057060e35ace3a2048
AbdumuminovAbduraxmon <abduraxmonabdumuminov77@gmail.com>
2022-12-11 13:06:13 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var str = alert('Добро пожаловать!')
+var place = alert('Разрешите дать свое местоположение?')
+var pic = alert('Даёте разрешение к галереий?')
+var name = prompt('Напишите свою имю!')
+var fam = prompt('Напишите свою фамилию!')
+var age = prompt('Напишите свой возраст!')
+
+console.log('name');
+console.log('fam');
+console.log('age');
